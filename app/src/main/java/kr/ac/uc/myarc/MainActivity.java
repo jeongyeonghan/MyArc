6127380baaef3da34cc48ae53aeb3059b9408379
jeongyeonghan <a2588407@naver.com>
2021-05-12 17:52:59 +0900

    GraphicText fist

---
+++ app/src/main/java/kr/ac/uc/myarc/MainActivity.java
@@ -0,0 +1,58 @@
+package kr.ac.uc.myarc;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+class MyView extends View {
+    private Paint mPaints, mFramePaint;
+    private RectF mBigOval;
+    private float mStart, mSweep;
+    private static final float SWEEP_INC = 2;
+    private static final float START_INC = 15;
+
+    public MyView(Context context){
+        super(context);
+
+        mPaints = new Paint();
+        mPaints.setAntiAlias(true);
+        mPaints.setStyle(Paint.Style.FILL);
+        mPaints.setColor(0x88ff0000);
+
+        mFramePaint = new Paint();
+        mFramePaint.setAntiAlias(true);
+        mFramePaint.setStyle(Paint.Style.STROKE);
+        mFramePaint.setStrokeWidth(3);
+        mFramePaint.setColor(0x8800ff00);
+        mBigOval = new RectF(40,10,900,1000);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.YELLOW);
+        canvas.drawRect(mBigOval, mFramePaint);
+        canvas.drawArc(mBigOval, mStart, mSweep, false, mPaints);
+        mSweep += SWEEP_INC;
+        if(mSweep > 360){
+            mSweep = mSweep - 360;
+            mStart = mStart + START_INC;
+            if(mStart >=360){
+                mStart = mStart - 360;
+            }
+        }
+        invalidate();
+    }
+}
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyView(this));
+    }
+}
